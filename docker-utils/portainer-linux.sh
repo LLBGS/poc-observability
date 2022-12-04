db4fd4b9a0d7c2dc992bb26ee9e5e9f865774881
LLBGS <lucasllbgs@gmail.com>
2022-12-04 10:19:06 -0300

    chore: create script to install portainer

--- docker-utils/portainer-linux.sh
+++ docker-utils/portainer-linux.sh
@@ -4,12 +4,12 @@
 
 
 
-# After installing Portainer, you can access it at http://localhost:9000
+# After installing Portainer, you can access it at http://localhost:9443
 # Case you want to access using wsl check this link: https://docs.portainer.io/start/install/server/docker/wsl
 
 # Create volume for Portainer
 docker volume create portainer_data
 
 # Run Portainer
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
