7f975daf5067320388c9fa08934b8e59341a7bc1
LLBGS <lucasllbgs@gmail.com>
2022-12-04 10:20:45 -0300

    feat: start api-gateway ms

---
+++ packages/api-gateway/src/config/producer.ts
@@ -0,0 +1,64 @@
+import { Kafka, logCreator, logLevel, Producer, ProducerBatch } from "kafkajs";
+
+interface CustomMessageFormat {
+  a: string;
+}
+
+interface Message {
+  key: string;
+  value: string;
+}
+
+interface TopicMessages {
+  topic: string;
+  messages: Message[];
+}
+
+export default class ProducerFactory {
+  private producer: Producer;
+
+  constructor() {
+    this.producer = this.createProducer();
+  }
+
+  public async start(): Promise<void> {
+    try {
+      await this.producer.connect();
+    } catch (error) {
+      console.log("Error connecting the producer: ", error);
+    }
+  }
+
+  public async shutdown(): Promise<void> {
+    await this.producer.disconnect();
+  }
+
+  public async sendBatch(messages: Array<CustomMessageFormat>): Promise<void> {
+    const kafkaMessages: Array<Message> = messages.map((message) => {
+      return {
+        key: "key",
+        value: JSON.stringify(message),
+      };
+    });
+
+    const topicMessages: TopicMessages = {
+      topic: "producer-topic",
+      messages: kafkaMessages,
+    };
+
+    const batch: ProducerBatch = {
+      topicMessages: [topicMessages],
+    };
+
+    await this.producer.sendBatch(batch);
+  }
+
+  private createProducer(): Producer {
+    const kafka = new Kafka({
+      clientId: "producer-client",
+      brokers: ["localhost:9092"],
+    });
+
+    return kafka.producer();
+  }
+}
---
+++ packages/api-gateway/src/main.ts
@@ -0,0 +1,25 @@
+import "module-alias/register";
+require("dotenv").config({ path: "../.env" });
+import appConfig from "./config/root.config";
+import { Express } from "express";
+import { closeConnection } from "./infra/database/database";
+
+const express = require("express");
+const newrelic = require("newrelic");
+const app: Express = express();
+const port = 3000;
+
+appConfig(app);
+newrelic.instrumentLoadedModule("express", express);
+const server = app.listen(port, () => {
+  console.log(`User V1 listining in port ${port}`);
+});
+
+process.on("SIGTERM" || "SIGINT", () => {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close(() => {
+    closeConnection();
+  });
+});
+
+export default app;
